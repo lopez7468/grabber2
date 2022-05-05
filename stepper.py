cde6e1c025e9d1c35aed8ce29fb45195ec499d23
lopez7468 <60163629+lopez7468@users.noreply.github.com>
2022-05-04 20:00:54 -0700

    Create stepper.py

---
+++ stepper.py
@@ -0,0 +1,64 @@
+from machine import Pin
+from time import sleep_us, sleep
+from hcsr04 import HCSR04
+
+dir1 = Pin(19, Pin.OUT)
+step = Pin(18, Pin.OUT)
+
+STEPDELAY_USECS = 1000
+STEPS_PER_REV = 200
+
+sensor = HCSR04(trigger_pin=17, echo_pin=16)
+
+gp0 = Pin(0, Pin.OUT)
+trigger = Pin(17)
+echo = Pin(16)
+
+def move():
+    
+    distance = sensor.distance_cm()
+    """
+    if distance < 4:
+        gp0.on()
+        return True
+        """
+    print(distance)
+   # print(trigger.value())
+   # print(echo.value())
+
+
+step_count = 0
+    
+def steps(delay_usecs):
+    
+    step.on()
+    sleep_us(delay_usecs)
+    step.off()
+    sleep_us(delay_usecs)
+         
+def go_back(step_count, delay_usecs):
+    dir1.on()
+    for i in range(step_count):
+        step.on()
+        sleep_us(delay_usecs)
+        step.off()
+        sleep_us(delay_usecs)
+    dir1.off()
+    
+           
+
+# example usage
+while True:
+    distance = sensor.distance_cm()
+    if distance > 4:
+        print(sensor.distance_cm())
+        print(step_count)
+        steps(STEPDELAY_USECS)
+        step_count = step_count + 1
+        sleep(0.00005)
+    if distance < 4:
+        print(step_count)
+        go_back(step_count, STEPDELAY_USECS)
+        break
+
+
